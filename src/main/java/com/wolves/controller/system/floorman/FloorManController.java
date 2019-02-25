3d53157fde8c06113e097bbccd11bd5c56d7625f
tanjian835296693@126.com <tanjian835296693@126.com>
2019-02-26 00:10:22 +0800

    层长代码

---
+++ src/main/java/com/wolves/controller/system/floorman/FloorManController.java
@@ -0,0 +1,242 @@
+package com.wolves.controller.system.floorman;
+
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+
+import com.wolves.controller.base.BaseController;
+import com.wolves.entity.Page;
+import com.wolves.service.system.buildman.BuildManService;
+import com.wolves.service.system.floorman.FloorManService;
+import com.wolves.util.*;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/** 
+ * 类名称：FloorManController
+ * 创建人：FH 
+ * 创建时间：2019-02-23
+ */
+@Controller
+@RequestMapping(value="/floorman")
+public class FloorManController extends BaseController {
+	
+	String menuUrl = "floorman/list.do"; //菜单地址(权限用)
+	@Resource(name="floormanService")
+	private FloorManService floormanService;
+	
+	/**
+	 * 新增
+	 */
+	@RequestMapping(value="/save")
+	public ModelAndView save() throws Exception{
+		logBefore(logger, "新增FloorMan");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "add")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd.put("FLOORMAN_ID", this.get32UUID());	//主键
+		floormanService.save(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	/**
+	 * 删除
+	 */
+	@RequestMapping(value="/delete")
+	public void delete(PrintWriter out){
+		logBefore(logger, "删除FloorMan");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "del")){return;} //校验权限
+		PageData pd = new PageData();
+		try{
+			pd = this.getPageData();
+			floormanService.delete(pd);
+			out.write("success");
+			out.close();
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		
+	}
+	
+	/**
+	 * 修改
+	 */
+	@RequestMapping(value="/edit")
+	public ModelAndView edit() throws Exception{
+		logBefore(logger, "修改FloorMan");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "edit")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		floormanService.edit(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	/**
+	 * 列表
+	 */
+	@RequestMapping(value="/list")
+	public ModelAndView list(Page page){
+		logBefore(logger, "列表FloorMan");
+		//if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		try{
+			pd = this.getPageData();
+			page.setPd(pd);
+			List<PageData>	varList = floormanService.list(page);	//列出FloorMan列表
+			mv.setViewName("system/floorman/floorman_list");
+			mv.addObject("varList", varList);
+			mv.addObject("pd", pd);
+			mv.addObject(Const.SESSION_QX,this.getHC());	//按钮权限
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		return mv;
+	}
+	
+	/**
+	 * 去新增页面
+	 */
+	@RequestMapping(value="/goAdd")
+	public ModelAndView goAdd(){
+		logBefore(logger, "去新增FloorMan页面");
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try {
+			mv.setViewName("system/floorman/floorman_edit");
+			mv.addObject("msg", "save");
+			mv.addObject("pd", pd);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		}						
+		return mv;
+	}	
+	
+	/**
+	 * 去修改页面
+	 */
+	@RequestMapping(value="/goEdit")
+	public ModelAndView goEdit(){
+		logBefore(logger, "去修改FloorMan页面");
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try {
+			pd = floormanService.findById(pd);	//根据ID读取
+			mv.setViewName("system/floorman/floorman_edit");
+			mv.addObject("msg", "edit");
+			mv.addObject("pd", pd);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		}						
+		return mv;
+	}	
+	
+	/**
+	 * 批量删除
+	 */
+	@RequestMapping(value="/deleteAll")
+	@ResponseBody
+	public Object deleteAll() {
+		logBefore(logger, "批量删除FloorMan");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "dell")){return null;} //校验权限
+		PageData pd = new PageData();		
+		Map<String,Object> map = new HashMap<String,Object>();
+		try {
+			pd = this.getPageData();
+			List<PageData> pdList = new ArrayList<PageData>();
+			String DATA_IDS = pd.getString("DATA_IDS");
+			if(null != DATA_IDS && !"".equals(DATA_IDS)){
+				String ArrayDATA_IDS[] = DATA_IDS.split(",");
+				floormanService.deleteAll(ArrayDATA_IDS);
+				pd.put("msg", "ok");
+			}else{
+				pd.put("msg", "no");
+			}
+			pdList.add(pd);
+			map.put("list", pdList);
+		} catch (Exception e) {
+			logger.error(e.toString(), e);
+		} finally {
+			logAfter(logger);
+		}
+		return AppUtil.returnObject(pd, map);
+	}
+	
+	/*
+	 * 导出到excel
+	 * @return
+	 */
+	@RequestMapping(value="/excel")
+	public ModelAndView exportExcel(){
+		logBefore(logger, "导出FloorMan到excel");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;}
+		ModelAndView mv = new ModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		try{
+			Map<String,Object> dataMap = new HashMap<String,Object>();
+			List<String> titles = new ArrayList<String>();
+			titles.add("id");	//1
+			titles.add("楼栋号");	//2
+			titles.add(" 楼层");	//3
+			titles.add("层长姓名");	//4
+			titles.add("联系方式");	//5
+			titles.add("创建时间");	//6
+			dataMap.put("titles", titles);
+			List<PageData> varOList = floormanService.listAll(pd);
+			List<PageData> varList = new ArrayList<PageData>();
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", varOList.get(i).get("ID").toString());	//1
+				vpd.put("var2", varOList.get(i).getString("BUILD_NO"));	//2
+				vpd.put("var3", varOList.get(i).getString("FLOOR"));	//3
+				vpd.put("var4", varOList.get(i).getString("FLOOR_MASTER_NAME"));	//4
+				vpd.put("var5", varOList.get(i).getString("MASTER_TEL"));	//5
+				vpd.put("var6", varOList.get(i).getString("CREATE_TIME"));	//6
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+		} catch(Exception e){
+			logger.error(e.toString(), e);
+		}
+		return mv;
+	}
+	
+	/* ===============================权限================================== */
+	public Map<String, String> getHC(){
+		Subject currentUser = SecurityUtils.getSubject();  //shiro管理的session
+		Session session = currentUser.getSession();
+		return (Map<String, String>)session.getAttribute(Const.SESSION_QX);
+	}
+	/* ===============================权限================================== */
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format,true));
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/system/floorman/floorman_edit.jsp
@@ -0,0 +1,158 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<base href="<%=basePath%>">
+		<meta charset="utf-8" />
+		<title></title>
+		<meta name="description" content="overview & stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="static/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="static/css/bootstrap-responsive.min.css" rel="stylesheet" />
+		<link rel="stylesheet" href="static/css/font-awesome.min.css" />
+		<!-- 下拉框 -->
+		<link rel="stylesheet" href="static/css/chosen.css" />
+		<link rel="stylesheet" href="static/css/ace.min.css" />
+		<link rel="stylesheet" href="static/css/ace-responsive.min.css" />
+		<link rel="stylesheet" href="static/css/ace-skins.min.css" />
+		<link rel="stylesheet" href="static/css/datepicker.css" /><!-- 日期框 -->
+		<script type="text/javascript" src="static/js/jquery-1.7.2.js"></script>
+		<script type="text/javascript" src="static/js/jquery.tips.js"></script>
+<script type="text/javascript">
+	//保存
+	function save(){
+			if($("#ID").val()==""){
+			$("#ID").tips({
+				side:3,
+	            msg:'请输入id',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#ID").focus();
+			return false;
+		}
+		if($("#BUILD_NO").val()==""){
+			$("#BUILD_NO").tips({
+				side:3,
+	            msg:'请输入楼栋号',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#BUILD_NO").focus();
+			return false;
+		}
+		if($("#FLOOR").val()==""){
+			$("#FLOOR").tips({
+				side:3,
+	            msg:'请输入 楼层',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#FLOOR").focus();
+			return false;
+		}
+		if($("#FLOOR_MASTER_NAME").val()==""){
+			$("#FLOOR_MASTER_NAME").tips({
+				side:3,
+	            msg:'请输入层长姓名',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#FLOOR_MASTER_NAME").focus();
+			return false;
+		}
+		if($("#MASTER_TEL").val()==""){
+			$("#MASTER_TEL").tips({
+				side:3,
+	            msg:'请输入联系方式',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#MASTER_TEL").focus();
+			return false;
+		}
+		if($("#CREATE_TIME").val()==""){
+			$("#CREATE_TIME").tips({
+				side:3,
+	            msg:'请输入创建时间',
+	            bg:'#AE81FF',
+	            time:2
+	        });
+			$("#CREATE_TIME").focus();
+			return false;
+		}
+		$("#Form").submit();
+		$("#zhongxin").hide();
+		$("#zhongxin2").show();
+	}
+	
+</script>
+	</head>
+<body>
+	<form action="floorman/${msg }.do" name="Form" id="Form" method="post">
+		<input type="hidden" name="FLOORMAN_ID" id="FLOORMAN_ID" value="${pd.FLOORMAN_ID}"/>
+		<div id="zhongxin">
+		<table id="table_report" class="table table-striped table-bordered table-hover">
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;">id:</td>
+				<td><input type="number" name="ID" id="ID" value="${pd.ID}" maxlength="32" placeholder="这里输入id" title="id"/></td>
+			</tr>
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;">楼栋号:</td>
+				<td><input type="text" name="BUILD_NO" id="BUILD_NO" value="${pd.BUILD_NO}" maxlength="32" placeholder="这里输入楼栋号" title="楼栋号"/></td>
+			</tr>
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;"> 楼层:</td>
+				<td><input type="text" name="FLOOR" id="FLOOR" value="${pd.FLOOR}" maxlength="32" placeholder="这里输入 楼层" title=" 楼层"/></td>
+			</tr>
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;">层长姓名:</td>
+				<td><input type="text" name="FLOOR_MASTER_NAME" id="FLOOR_MASTER_NAME" value="${pd.FLOOR_MASTER_NAME}" maxlength="32" placeholder="这里输入层长姓名" title="层长姓名"/></td>
+			</tr>
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;">联系方式:</td>
+				<td><input type="text" name="MASTER_TEL" id="MASTER_TEL" value="${pd.MASTER_TEL}" maxlength="32" placeholder="这里输入联系方式" title="联系方式"/></td>
+			</tr>
+			<tr>
+				<td style="width:70px;text-align: right;padding-top: 13px;">创建时间:</td>
+				<td><input class="span10 date-picker" name="CREATE_TIME" id="CREATE_TIME" value="${pd.CREATE_TIME}" type="text" data-date-format="yyyy-mm-dd" readonly="readonly" placeholder="创建时间" title="创建时间"/></td>
+			</tr>
+			<tr>
+				<td style="text-align: center;" colspan="10">
+					<a class="btn btn-mini btn-primary" onclick="save();">保存</a>
+					<a class="btn btn-mini btn-danger" onclick="top.Dialog.close();">取消</a>
+				</td>
+			</tr>
+		</table>
+		</div>
+		
+		<div id="zhongxin2" class="center" style="display:none"><br/><br/><br/><br/><br/><img src="static/images/jiazai.gif" /><br/><h4 class="lighter block green">提交中...</h4></div>
+		
+	</form>
+	
+	
+		<!-- 引入 -->
+		<script type="text/javascript">window.jQuery || document.write("<script src='static/js/jquery-1.9.1.min.js'>\x3C/script>");</script>
+		<script src="static/js/bootstrap.min.js"></script>
+		<script src="static/js/ace-elements.min.js"></script>
+		<script src="static/js/ace.min.js"></script>
+		<script type="text/javascript" src="static/js/chosen.jquery.min.js"></script><!-- 下拉框 -->
+		<script type="text/javascript" src="static/js/bootstrap-datepicker.min.js"></script><!-- 日期框 -->
+		<script type="text/javascript">
+		$(top.hangge());
+		$(function() {
+			//单选框
+			$(".chzn-select").chosen(); 
+			$(".chzn-select-deselect").chosen({allow_single_deselect:true}); 
+			//日期框
+			$('.date-picker').datepicker();
+		});
+		</script>
+</body>
+</html>
