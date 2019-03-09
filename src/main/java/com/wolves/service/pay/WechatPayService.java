e64fb9c7d5c908bc3a7475bf79808a643f0a97ac
tanjian <tanjian835296693@126.com>
2019-03-09 15:11:54 +0800

    修改

--- src/main/java/com/wolves/service/pay/WechatPayService.java
+++ src/main/java/com/wolves/service/pay/WechatPayService.java
@@ -1,11 +1,15 @@
 package com.wolves.service.pay;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * @author xulu
  * @date 2019/2/28
  * @link https://github.com/xulu163
  */
 public class WechatPayService implements PayService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void pay() {
@@ -16,4 +20,5 @@ public class WechatPayService implements PayService {
     public void reback() {
 
     }
+
 }
